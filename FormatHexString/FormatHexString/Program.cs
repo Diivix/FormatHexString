74ec30a36ed8ab7b1b9a3778012d504014224cd9
Diivix2590 <Diivix2590@users.noreply.github.com>
2015-12-01 19:31:59 +1100

    fix from today
    Bug on line 35.

--- FormatHexString/FormatHexString/Program.cs
+++ FormatHexString/FormatHexString/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace FormatHexString
 {
@@ -6,34 +8,57 @@ namespace FormatHexString
     {
         static void Main(string[] args)
         {
-            string unFormattedString = "\\xE9 blah \\xfa balh \\xff";
+            string unFormattedString = "\\xc2\\xa7 kk";
             string formattedString = FormatHex2String(unFormattedString);
 
             Console.WriteLine(formattedString);
             Console.ReadLine();
         }
 
-        private static string FormatHex2String(string hexString)
+        private static string FormatHex2String(string inputString)
         {
             try
             {
-                while (hexString.Contains("\\x"))
+                while (inputString.Contains("\\x"))
                 {
-                    int startIndex = hexString.IndexOf("\\x") + 2;
-                    string subString = hexString.Substring(startIndex, 2);
+                    int startIndex = inputString.IndexOf("\\x");
+                    string hexString = string.Empty;
 
-                    int intValue = Convert.ToInt32(subString, 16);
-                    string stringValue = char.ConvertFromUtf32(intValue);
+                    int cursor = startIndex;
+                    bool isHexString = false;
+                    while(!isHexString)
+                    {
+                        hexString = hexString + inputString.Substring(cursor, 4);
+                        cursor += 4;
+
+                        //Exit, if the following values are not hex delimeted.
+                        if(!inputString.Substring(cursor, 2).Equals("\\x"))
+                        {
+                            isHexString = true;
+                        }
+                    }
+
+                    string hexCompressed = hexString.Replace("\\x", "");
+
+                    byte[] bytes = ASCIIEncoding.ASCII.GetBytes(hexCompressed);
+                    List<byte> byteList = new List<byte>();
+                    for (int i = 0; i <= bytes.Length - 2; i += 2)
+                    {
+                        int hex = Convert.ToInt32(ASCIIEncoding.ASCII.GetString(bytes, i, 2), 16);
+                        byteList.Add((byte)hex);
+                    }
+
+                    hexCompressed = ASCIIEncoding.UTF32.GetString(byteList.ToArray());
 
                     //replaces all of the same hex value occurances
-                    hexString = hexString.Replace("\\x" + subString, stringValue); 
+                    inputString = inputString.Replace(hexString, hexCompressed); 
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine("No go... {0}", e.Message);
             }
-            return hexString;
+            return inputString;
         }
     }
 }
